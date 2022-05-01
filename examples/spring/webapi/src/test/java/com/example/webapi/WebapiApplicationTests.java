612a9db7d3a2feabae6d460e1aa5625de5f1c9cc
Antony Baasandorj <antony.baasan@gmail.com>
2022-04-30 22:17:32 -0400

    add example spring project

---
+++ examples/spring/webapi/src/test/java/com/example/webapi/WebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
