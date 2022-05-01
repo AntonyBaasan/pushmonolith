ec2339dbae939af15c22ba2536d28b9d7a461ccb
Antony Baasandorj <antony.baasan@gmail.com>
2022-05-01 00:40:09 -0400

    update CF, use S3 to upload artifact

--- pushmonolith/core/src/services/test.service.ts
+++ pushmonolith/core/src/services/test.service.ts
@@ -2,21 +2,31 @@ import {
     CloudFormationClientConfig,
     CloudFormation,
     CreateStackCommandInput,
-    Stack
+    Stack,
+    Capability
 } from '@aws-sdk/client-cloudformation';
+import { S3Service } from './s3.service';
 import { BaseTemplate } from './template';
 
 export class TestService {
     public cloudformation: CloudFormation;
+    public s3Service: S3Service;
 
     constructor() {
+        this.s3Service = new S3Service();
         const config: CloudFormationClientConfig = {
             region: 'us-east-1'
         };
         this.cloudformation = new CloudFormation(config)
     }
 
+    public async saySomething2(): Promise<string> {
+        await this.s3Service.createBucket('C:\\Users\\abaasandorj\\git\\pushmonolith\\examples\\spring\\webapi\\target\\webapi-0.0.1-SNAPSHOT.jar');
+        return 'Done';
+    }
+
     public async saySomething(): Promise<string> {
+
         const createStackParam: CreateStackCommandInput = this.createEc2StackInput();
 
         const stack: Stack = await this.getStackInfo(createStackParam.StackName);
@@ -35,6 +45,7 @@ export class TestService {
         } catch (error) {
             return JSON.stringify(error);
         }
+
     }
 
     private async getStackInfo(stackName: string | undefined): Promise<Stack> {
@@ -63,6 +74,7 @@ export class TestService {
                 },
             ],
             TemplateBody: JSON.stringify(BaseTemplate),
+            Capabilities: [Capability.CAPABILITY_IAM]
         } as CreateStackCommandInput;
     }
 }
